b70b93f700770e3882ee0ac4ea10b8bbbe18168d
Andy Wermke <andy.dev@next-step-software.com>
2018-12-18 20:11:49 +0100

    Implement watch mode (#3)

--- src/cli.ts
+++ src/cli.ts
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+import * as chokidar from "chokidar"
 import * as logSymbols from "log-symbols"
 import meow from "meow"
 import { isAugmentedError } from "./errors"
@@ -9,33 +10,65 @@ import { assertNoBrokenColumnRefs, assertNoBrokenTableRefs } from "./validation"
 const cli = meow(`
   Usage
     $ pg-lint ./path/to/source/*.ts
-`)
 
-let allQueries: Query[] = []
-let allTableSchemas: TableSchema[] = []
-
-try {
-  for (const filePath of cli.input) {
-    const { queries, tableSchemas } = parseFile(filePath)
-    // TODO: Check that no table is defined twice
-
-    allQueries = [...allQueries, ...queries]
-    allTableSchemas = [...allTableSchemas, ...tableSchemas]
+  Options
+    --help        Print this help
+    -w, --watch   Watch files and re-evaluate on change
+`, {
+  flags: {
+    help: { type: "boolean" },
+    w: { type: "boolean" },
+    watch: { type: "boolean" }
   }
+})
 
-  for (const query of allQueries) {
-    assertNoBrokenTableRefs(query, allTableSchemas)
-    assertNoBrokenColumnRefs(query, allTableSchemas)
-  }
-} catch (error) {
-  if (isAugmentedError(error)) {
-    const [firstLine, ...lines] = String(error.message).split("\n")
-    const linesIndented = lines.map(line => line.match(/^>?\s*\d*\s*\|/) ? line : `  ${line}`)
-    console.error(`${logSymbols.error} ${[firstLine, ...linesIndented].join("\n")}`)
-  } else {
-    console.error(error.stack)
-  }
-  process.exit(1)
+if (cli.input.length === 0) {
+  cli.showHelp()
+  process.exit(0)
 }
 
-console.log(`${logSymbols.success} Validated ${allQueries.length} queries against ${allTableSchemas.length} table schemas. All fine!`)
+const watchMode = Boolean(cli.flags.watch || cli.flags.w)
+
+function run (sourceFilePaths: string[]) {
+  let allQueries: Query[] = []
+  let allTableSchemas: TableSchema[] = []
+
+  try {
+    for (const filePath of sourceFilePaths) {
+      const { queries, tableSchemas } = parseFile(filePath)
+      // TODO: Check that no table is defined twice
+
+      allQueries = [...allQueries, ...queries]
+      allTableSchemas = [...allTableSchemas, ...tableSchemas]
+    }
+
+    for (const query of allQueries) {
+      assertNoBrokenTableRefs(query, allTableSchemas)
+      assertNoBrokenColumnRefs(query, allTableSchemas)
+    }
+
+    console.log(`${logSymbols.success} Validated ${allQueries.length} queries against ${allTableSchemas.length} table schemas. All fine!`)
+  } catch (error) {
+    if (isAugmentedError(error)) {
+      const [firstLine, ...lines] = String(error.message).split("\n")
+      const linesIndented = lines.map(line => line.match(/^>?\s*\d*\s*\|/) ? line : `  ${line}`)
+      console.error(`${logSymbols.error} ${[firstLine, ...linesIndented].join("\n")}`)
+    } else {
+      console.error(error.stack)
+    }
+    if (!watchMode) {
+      process.exit(1)
+    }
+  }
+}
+
+run(cli.input)
+
+if (watchMode) {
+  console.log(`\nWatching file changes... Press CTRL + C to cancel.\n`)
+
+  chokidar.watch(cli.input).on("all", (event, filePath) => {
+    run([ filePath ])
+    console.log("")
+  })
+}
