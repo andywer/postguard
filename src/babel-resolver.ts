sqorn/pg-lint

Parse column types of schemas

src/babel-resolver.ts
@@ -1,0 +1,157 @@
+import { NodePath } from "@babel/traverse"
+import * as types from "@babel/types"
+import { ColumnDescriptor, Schema } from "sqldb/schema"
+import { getReferencedNamedImport } from "./babel-imports"
+
+const $descriptor = Symbol("Schema descriptor")
+
+export const isDescriptor = (thing: any) => thing && typeof thing === "object" && thing[$descriptor]
+
+export const markAsDescriptor = <T extends {}>(obj: T): T => {
+  Object.defineProperty(obj, $descriptor, {
+    enumerable: false,
+    value: true
+  })
+  return obj
+}
+
+function getColumnNameByKey(key: NodePath<types.Node>): string {
+  if (key.isIdentifier()) {
+    return key.node.name
+  } else {
+    const name = key.evaluate().value
+    if (!name) throw key.buildCodeFrameError("Cannot resolve property key value (column name).")
+    return name
+  }
+}
+
+function resolveCallExpression(
+  call: NodePath<types.CallExpression>,
+  fn: ((...args: any[]) => any),
+  args: NodePath[]
+): ColumnDescriptor | any {
+  const resolvedArgs = args.map(arg => resolveColumnDescriptorExpression(arg))
+
+  try {
+    const result = fn(...resolvedArgs)
+    return result && typeof result === "object" && result.type ? markAsDescriptor(result) : result
+  } catch (error) {
+    throw call.buildCodeFrameError(
+      `Call to ${call.get("callee").getSource()} failed: ${error.message}`
+    )
+  }
+}
+
+function resolveSchemaDotSomethingDescriptor(
+  name: string,
+  parentNodePath: NodePath
+): ColumnDescriptor | any {
+  const descriptor = (Schema as { [key: string]: any })[name]
+
+  if (!descriptor) throw parentNodePath.buildCodeFrameError(`Schema.${name} is not known.`)
+
+  if (typeof descriptor === "object") {
+    return markAsDescriptor(descriptor)
+  } else if (typeof descriptor === "function") {
+    return descriptor
+  } else {
+    throw parentNodePath.buildCodeFrameError(
+      `Schema.${name} is of unexpected type ${typeof descriptor}.`
+    )
+  }
+}
+
+function resolveSchemaDescriptorName(path: NodePath<types.MemberExpression>) {
+  const object = path.get("object")
+  const property = path.get("property")
+
+  if (!object.isIdentifier()) throw object.buildCodeFrameError("Expected Schema.*")
+  const schemaImport = getReferencedNamedImport(object, "Schema")
+
+  if (!schemaImport) {
+    throw object.buildCodeFrameError("Cannot parse schema. Please reference a Schema.* descriptor.")
+  }
+  if (Array.isArray(property) || !property.isIdentifier()) {
+    throw path.buildCodeFrameError("Cannot parse schema.")
+  }
+
+  return property.node.name
+}
+
+export function resolveColumnDescriptorExpression(
+  path: NodePath<types.Node>
+): ColumnDescriptor | any {
+  if (path.isIdentifier()) {
+    const binding = path.scope.getBinding(path.node.name)
+    if (!binding) throw path.buildCodeFrameError(`Cannot resolve variable "${path.node.name}".`)
+
+    if (binding.path.isVariableDeclarator()) {
+      const init = binding.path.get("init")
+      if (!init) {
+        throw binding.path.buildCodeFrameError(
+          `Expected ${binding.identifier.name} to reference some Schema.* descriptor.`
+        )
+      }
+      return resolveColumnDescriptorExpression(init as NodePath)
+    } else {
+      throw binding.path.buildCodeFrameError(
+        "Cannot parse referencing schema. Please reference a Schema.* descriptor."
+      )
+    }
+  } else if (path.isMemberExpression()) {
+    const propertyName = resolveSchemaDescriptorName(path)
+    return resolveSchemaDotSomethingDescriptor(propertyName, path)
+  } else if (path.isCallExpression()) {
+    const callee = path.get("callee")
+    const args = path.get("arguments")
+
+    const resolvedCallee = resolveColumnDescriptorExpression(callee)
+    if (typeof resolvedCallee !== "function") {
+      throw callee.buildCodeFrameError(
+        `Expected a function as callee, got ${typeof resolvedCallee}.`
+      )
+    }
+
+    return resolveCallExpression(path, resolvedCallee, args)
+  } else if (path.isArrayExpression()) {
+    const elements = path.get("elements")
+
+    for (const element of elements) {
+      if (element.isSpreadElement()) {
+        throw element.buildCodeFrameError("Array spread cannot be parsed.")
+      }
+    }
+
+    return elements.map(element => resolveColumnDescriptorExpression(element as NodePath))
+  } else if (path.isObjectExpression()) {
+    const resolved: { [key: string]: any } = {}
+
+    for (const property of path.get("properties")) {
+      if (!property.isObjectProperty()) {
+        throw property.buildCodeFrameError("Expected object property (no spread or method).")
+      }
+
+      const key = property.get("key")
+      const value = property.get("value")
+
+      if (Array.isArray(key)) {
+        throw property.buildCodeFrameError("Did not expect property key to be an array.")
+      }
+
+      const columnName = getColumnNameByKey(key)
+      resolved[columnName] = resolveColumnDescriptorExpression(value)
+    }
+
+    return resolved
+  } else {
+    const evaluated = path.evaluate()
+
+    if (evaluated.confident) {
+      return evaluated.value
+    }
+  }
+
+  throw path.buildCodeFrameError(
+    `Expected a Schema.* descriptor. Found an unhandled ${path.node.type} instead.`
+  )
+}

src/parse-file.ts
@@ -1,7 +1,8 @@
 import { parse, traverse } from "@babel/core"
 import createDebugLogger from "debug"
 import * as fs from "fs"
-import { getReferencedNamedImport } from "./babel-parser-utils"
+import { ColumnDescriptor } from "sqldb"
+import { getReferencedNamedImport } from "./babel-imports"
 import { fail } from "./errors"
 import { parseQuery } from "./parse-query"
 import { parseTableDefinition } from "./parse-table-definition"
@@ -20,6 +21,23 @@ function compileTypeScript(filePath: string) {
   } catch (error) {
     // tslint:disable-next-line no-console
     console.error(`Compiling TypeScript source file ${filePath} failed: ${error.message}`)
+  }
+}
+
+function stringifyColumnType(descriptor: ColumnDescriptor) {
+  const props: string[] = [
+    descriptor.hasDefault ? "default value" : null,
+    descriptor.nullable ? "nullable" : null
+  ].filter(str => !!str) as string[]
+
+  const propsString = props.length > 0 ? ` (${props.join(", ")})` : ""
+
+  if (descriptor.type === "enum" && descriptor.enum) {
+    return `enum${propsString} [${descriptor.enum.map(value => `'${value}'`).join(", ")}]`
+  } else {
+    return `${descriptor.type}${
+      descriptor.subtype ? `[${descriptor.subtype.type}]` : ""
+    }${propsString}`
   }
 }
 
@@ -79,12 +97,15 @@ export function parseSourceFile(sourceFile: SourceFile) {
     const formattedColumnRefs = query.referencedColumns.map(col =>
       "tableName" in col ? `${col.tableName}.${col.columnName}` : col.columnName
     )
-    debugQueries(
-      `  Query: ${query.query}\n    Referenced columns: ${formattedColumnRefs.join(", ")}`
-    )
+    const referencedColumns = formattedColumnRefs.length > 0 ? formattedColumnRefs.join(", ") : "-"
+    debugQueries(`  Query: ${query.query.trim()}\n    Referenced columns: ${referencedColumns}`)
   }
   for (const table of tableSchemas) {
-    debugTables(`  Table: ${table.tableName}\n    Columns: ${table.columnNames.join(", ")}`)
+    debugTables(`  Table: ${table.tableName}`)
+    for (const columnName of table.columnNames) {
+      const columnType = stringifyColumnType(table.columnDescriptors[columnName])
+      debugTables(`    Column "${columnName}": ${columnType}`)
+    }
   }
 
   return {

src/types.ts
@@ -1,4 +1,5 @@
 import * as types from "@babel/types"
+import { TableSchemaDescriptor } from "sqldb"
 import * as ts from "typescript"
 import { QueryNodePath } from "./query-parser-utils"
 
@@ -15,6 +16,7 @@ export interface TableSchema {
   sourceFile: SourceFile
   tableName: string
   columnNames: string[]
+  columnDescriptors: TableSchemaDescriptor
   loc: types.SourceLocation | null
 }
 

andywer/postguard

src/babel-resolver.ts
@@ -1,0 +1,157 @@
+import { NodePath } from "@babel/traverse"
+import * as types from "@babel/types"
+import { ColumnDescriptor, Schema } from "squid/schema"
+import { getReferencedNamedImport } from "./babel-imports"
+
+const $descriptor = Symbol("Schema descriptor")
+
+export const isDescriptor = (thing: any) => thing && typeof thing === "object" && thing[$descriptor]
+
+export const markAsDescriptor = <T extends {}>(obj: T): T => {
+  Object.defineProperty(obj, $descriptor, {
+    enumerable: false,
+    value: true
+  })
+  return obj
+}
+
+function getColumnNameByKey(key: NodePath<types.Node>): string {
+  if (key.isIdentifier()) {
+    return key.node.name
+  } else {
+    const name = key.evaluate().value
+    if (!name) throw key.buildCodeFrameError("Cannot resolve property key value (column name).")
+    return name
+  }
+}
+
+function resolveCallExpression(
+  call: NodePath<types.CallExpression>,
+  fn: ((...args: any[]) => any),
+  args: NodePath[]
+): ColumnDescriptor | any {
+  const resolvedArgs = args.map(arg => resolveColumnDescriptorExpression(arg))
+
+  try {
+    const result = fn(...resolvedArgs)
+    return result && typeof result === "object" && result.type ? markAsDescriptor(result) : result
+  } catch (error) {
+    throw call.buildCodeFrameError(
+      `Call to ${call.get("callee").getSource()} failed: ${error.message}`
+    )
+  }
+}
+
+function resolveSchemaDotSomethingDescriptor(
+  name: string,
+  parentNodePath: NodePath
+): ColumnDescriptor | any {
+  const descriptor = (Schema as { [key: string]: any })[name]
+
+  if (!descriptor) throw parentNodePath.buildCodeFrameError(`Schema.${name} is not known.`)
+
+  if (typeof descriptor === "object") {
+    return markAsDescriptor(descriptor)
+  } else if (typeof descriptor === "function") {
+    return descriptor
+  } else {
+    throw parentNodePath.buildCodeFrameError(
+      `Schema.${name} is of unexpected type ${typeof descriptor}.`
+    )
+  }
+}
+
+function resolveSchemaDescriptorName(path: NodePath<types.MemberExpression>) {
+  const object = path.get("object")
+  const property = path.get("property")
+
+  if (!object.isIdentifier()) throw object.buildCodeFrameError("Expected Schema.*")
+  const schemaImport = getReferencedNamedImport(object, "Schema")
+
+  if (!schemaImport) {
+    throw object.buildCodeFrameError("Cannot parse schema. Please reference a Schema.* descriptor.")
+  }
+  if (Array.isArray(property) || !property.isIdentifier()) {
+    throw path.buildCodeFrameError("Cannot parse schema.")
+  }
+
+  return property.node.name
+}
+
+export function resolveColumnDescriptorExpression(
+  path: NodePath<types.Node>
+): ColumnDescriptor | any {
+  if (path.isIdentifier()) {
+    const binding = path.scope.getBinding(path.node.name)
+    if (!binding) throw path.buildCodeFrameError(`Cannot resolve variable "${path.node.name}".`)
+
+    if (binding.path.isVariableDeclarator()) {
+      const init = binding.path.get("init")
+      if (!init) {
+        throw binding.path.buildCodeFrameError(
+          `Expected ${binding.identifier.name} to reference some Schema.* descriptor.`
+        )
+      }
+      return resolveColumnDescriptorExpression(init as NodePath)
+    } else {
+      throw binding.path.buildCodeFrameError(
+        "Cannot parse referencing schema. Please reference a Schema.* descriptor."
+      )
+    }
+  } else if (path.isMemberExpression()) {
+    const propertyName = resolveSchemaDescriptorName(path)
+    return resolveSchemaDotSomethingDescriptor(propertyName, path)
+  } else if (path.isCallExpression()) {
+    const callee = path.get("callee")
+    const args = path.get("arguments")
+
+    const resolvedCallee = resolveColumnDescriptorExpression(callee)
+    if (typeof resolvedCallee !== "function") {
+      throw callee.buildCodeFrameError(
+        `Expected a function as callee, got ${typeof resolvedCallee}.`
+      )
+    }
+
+    return resolveCallExpression(path, resolvedCallee, args)
+  } else if (path.isArrayExpression()) {
+    const elements = path.get("elements")
+
+    for (const element of elements) {
+      if (element.isSpreadElement()) {
+        throw element.buildCodeFrameError("Array spread cannot be parsed.")
+      }
+    }
+
+    return elements.map(element => resolveColumnDescriptorExpression(element as NodePath))
+  } else if (path.isObjectExpression()) {
+    const resolved: { [key: string]: any } = {}
+
+    for (const property of path.get("properties")) {
+      if (!property.isObjectProperty()) {
+        throw property.buildCodeFrameError("Expected object property (no spread or method).")
+      }
+
+      const key = property.get("key")
+      const value = property.get("value")
+
+      if (Array.isArray(key)) {
+        throw property.buildCodeFrameError("Did not expect property key to be an array.")
+      }
+
+      const columnName = getColumnNameByKey(key)
+      resolved[columnName] = resolveColumnDescriptorExpression(value)
+    }
+
+    return resolved
+  } else {
+    const evaluated = path.evaluate()
+
+    if (evaluated.confident) {
+      return evaluated.value
+    }
+  }
+
+  throw path.buildCodeFrameError(
+    `Expected a Schema.* descriptor. Found an unhandled ${path.node.type} instead.`
+  )
+}

src/parse-file.ts
@@ -1,7 +1,8 @@
 import { parse, traverse } from "@babel/core"
 import createDebugLogger from "debug"
 import * as fs from "fs"
-import { getReferencedNamedImport } from "./babel-parser-utils"
+import { ColumnDescriptor } from "squid"
+import { getReferencedNamedImport } from "./babel-imports"
 import { fail } from "./errors"
 import { parseQuery } from "./parse-query"
 import { parseTableDefinition } from "./parse-table-definition"
@@ -20,6 +21,23 @@ function compileTypeScript(filePath: string) {
   } catch (error) {
     // tslint:disable-next-line no-console
     console.error(`Compiling TypeScript source file ${filePath} failed: ${error.message}`)
+  }
+}
+
+function stringifyColumnType(descriptor: ColumnDescriptor) {
+  const props: string[] = [
+    descriptor.hasDefault ? "default value" : null,
+    descriptor.nullable ? "nullable" : null
+  ].filter(str => !!str) as string[]
+
+  const propsString = props.length > 0 ? ` (${props.join(", ")})` : ""
+
+  if (descriptor.type === "enum" && descriptor.enum) {
+    return `enum${propsString} [${descriptor.enum.map(value => `'${value}'`).join(", ")}]`
+  } else {
+    return `${descriptor.type}${
+      descriptor.subtype ? `[${descriptor.subtype.type}]` : ""
+    }${propsString}`
   }
 }
 
@@ -79,12 +97,15 @@ export function parseSourceFile(sourceFile: SourceFile) {
     const formattedColumnRefs = query.referencedColumns.map(col =>
       "tableName" in col ? `${col.tableName}.${col.columnName}` : col.columnName
     )
-    debugQueries(
-      `  Query: ${query.query}\n    Referenced columns: ${formattedColumnRefs.join(", ")}`
-    )
+    const referencedColumns = formattedColumnRefs.length > 0 ? formattedColumnRefs.join(", ") : "-"
+    debugQueries(`  Query: ${query.query.trim()}\n    Referenced columns: ${referencedColumns}`)
   }
   for (const table of tableSchemas) {
-    debugTables(`  Table: ${table.tableName}\n    Columns: ${table.columnNames.join(", ")}`)
+    debugTables(`  Table: ${table.tableName}`)
+    for (const columnName of table.columnNames) {
+      const columnType = stringifyColumnType(table.columnDescriptors[columnName])
+      debugTables(`    Column "${columnName}": ${columnType}`)
+    }
   }
 
   return {

src/types.ts
@@ -1,4 +1,5 @@
 import * as types from "@babel/types"
+import { TableSchemaDescriptor } from "squid"
 import * as ts from "typescript"
 import { QueryNodePath } from "./query-parser-utils"
 
@@ -15,6 +16,7 @@ export interface TableSchema {
   sourceFile: SourceFile
   tableName: string
   columnNames: string[]
+  columnDescriptors: TableSchemaDescriptor
   loc: types.SourceLocation | null
 }
 