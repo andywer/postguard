227a53e69da165e06cb8915b1113b089568f2066
Caine <36860492+drpym@users.noreply.github.com>
2021-04-05 16:35:35 -0700

    properly named UUID field, and is readable by postguard

--- test/_fixtures/schema.ts
+++ test/_fixtures/schema.ts
@@ -10,6 +10,7 @@ defineTable("users", {
       weblink: Schema.nullable(Schema.String)
     })
   ),
+  uuid: Schema.UUID,
   created_at: Schema.default(Schema.Date),
   updated_at: Schema.nullable(Schema.Date),
   roles: Schema.Array(Schema.Enum(["admin", "user"]))
